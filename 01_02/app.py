59e04e10c278a84b35d9fc5de40aa9119f33d81c
Lincy <lincgr@gmail.com>
2023-12-16 17:03:40 -0600

    01_02 Crear series con etiquetas en pandas

---
+++ 01_02/app.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+lista_email = ["hmaldonado@kinetecoinc.com","czambrano@kinetecoinc.com","edomínguez@kinetecoinc.com","tbenítez@kinetecoinc.com"]
+serie_email = pd.Series(lista_email)
+
+print(serie_email)
