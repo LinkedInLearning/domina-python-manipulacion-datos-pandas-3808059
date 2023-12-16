728fd199531253076478d99e448943f4337b5afa
Lincy <lincgr@gmail.com>
2023-12-16 16:26:45 -0600

    01_01 Convertir un diccionario a una serie en Pandas

---
+++ 01_01/app.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+diccionario_empleados = {
+    15379 : "hmaldonado@kinetecoinc.com",
+    15419 : "czambrano@kinetecoinc.com",
+    15420 : "edomínguez@kinetecoinc.com",
+    46556 : "tbenítez@kinetecoinc.com",
+    46555 : "sblanco@kinetecoinc.com",
+    46559 : "egarcía@kinetecoinc.com",
+    15428 : "rtorres@kinetecoinc.com",
+    15429 : "rmolina@kinetecoinc.com",
+    47130 : "nflores@kinetecoinc.com",
+    32117 : "pbermúdez@kinetecoinc.com",
+    32313 : "cortiz@kinetecoinc.com"
+}
