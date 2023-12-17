e124bcf363d31771f41591e37a760e0f2e4d9ac0
Lincy <lincgr@gmail.com>
2023-12-17 15:08:37 -0600

    01_07 Funciones de agregación en Series de Pandas

---
+++ 01_07/app.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+salariosAnuales = [50230.0, 55123.0, 60089.0, 75020.0, 100987.0, 202345.0, 89000.0, 61200.0, 55300.0, 73400.0, 9520.0, 870000.0]
+serie_salarios = pd.Series(salariosAnuales)
+print(serie_salarios)
