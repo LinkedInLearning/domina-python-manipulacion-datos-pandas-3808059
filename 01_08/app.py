835dd421775acc89d2f3ed7af01aece1c70df662
Lincy <lincgr@gmail.com>
2023-12-17 16:19:52 -0600

    01_08 Agregar nueva columna a un dataFrame en Pandas

---
+++ 01_08/app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df_clientes = pd.DataFrame({
+  "nombre" : ["Luis", "Ana", "Sebastián", "Sofía", "Mateo", "Camila", "Valentín", "Isabella"],
+  "edad" : [35, 30, 32, 41, 47, 23, 20, 28], 
+  "cuidad" : ["Buenos Aires", "Lima", "Ciudad de México", "Santiago", "Alajuela", "Nueva York", "Toronto", "Río de Janeiro"]
+})
+
+lista_profesion = ["Médico", "Ingeniera civil", "Abogado", "Profesora", "Contador", "Enfermera", "Estudiante", "Periodista"]
