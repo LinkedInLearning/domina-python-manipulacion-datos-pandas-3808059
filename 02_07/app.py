54baddfcdafe623c8965c4e7e95c77869c0cd8fa
Lincy <lincgr@gmail.com>
2023-12-17 18:58:45 -0600

    02_07 Exportar un dataFrame de Pandas en un archivo CSV

---
+++ 02_07/app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df_clientes = pd.DataFrame({
+  "nombre" : ["Luis", "Ana", "Sebastián", "Sofía", "Mateo", "Camila", "Valentín", "Isabella"],
+  "cuidad" : ["Buenos Aires", "Lima", "Ciudad de México", "Santiago", "Alajuela", "Nueva York", "Toronto", "Río de Janeiro"], 
+  "profesion": ["Médico", "Ingeniera civil", "Abogado", "Profesora", "Contador", "Enfermera", "Estudiante", "Periodista"]
+})
+
+archivo = 'clientes.csv'
