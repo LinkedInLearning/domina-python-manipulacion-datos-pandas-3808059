3f61613405d5b812cc1b62d34652f5e6ae61e7fb
Lincy <lincgr@gmail.com>
2023-12-19 20:57:07 -0600

    02_11 "Reestructurar un dataFrame en Pandas "

---
+++ 02_11/app.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df_ventas_semestre = pd.DataFrame({
+    'productos': ['computadora', 'teléfono', 'computadora', 'teléfono', 'computadora', 'teléfono', 'computadora', 'teléfono' ],
+    'mes': ['Enero', 'Enero', 'Febrero', 'Febrero', 'Marzo', 'Marzo', 'Abril', 'Abril'],
+    'ventas': [10000, 12000, 15000, 18000, 20000, 22000, 15000, 18000]
+})
+
+print("Dataframe original:")
+print(df_ventas_semestre)
