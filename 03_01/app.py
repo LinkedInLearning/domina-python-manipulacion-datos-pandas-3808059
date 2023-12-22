8aac0b134e32c196c0ad167d14fd7204ac2a3055
Lincy <lincgr@gmail.com>
2023-12-21 20:39:59 -0600

    03_01 Gráfico de barras de un dataFrame en Pandas

---
+++ 03_01/app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df_ventas = pd.DataFrame({
+    'productos': ['computadora', 'teléfono', 'relojes', 'televisores', 'computadora', 'cámaras', 'computadora', 'teléfono' ],
+    'ventas': [10000, 12000, 15000, 18000, 20000, 22000, 15000, 18000]
+})
