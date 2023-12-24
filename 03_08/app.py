c4f467ffc30c1da78f83267bab65df1ae28c6c25
Lincy <lincgr@gmail.com>
2023-12-24 09:52:59 -0600

    03_08 Tablas dinámicas en Pandas

---
+++ 03_08/app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df_ventas = pd.DataFrame({
+    "producto": ["Computadora", "Camiseta", "Alfombras", "Tableta", "Vestido", "Computadora"],
+    "categoria": ["Electrónicos", "Ropa", "Hogar", "Electrónicos", "Ropa", "Electrónicos"],
+    "ventas": [10000, 12000, 15000, 18000, 2000, 3000],
+})
